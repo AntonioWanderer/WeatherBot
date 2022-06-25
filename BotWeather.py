657da8effc1164f39f1ac08b9a5314919f397303
AntonioWanderer <areikow@mail.ru>
2022-06-26 02:31:01 +0700

    Bot polling created

--- BotWeather.py
+++ BotWeather.py
@@ -1,8 +1,20 @@
-import GetWeather as prs
+import GetWeather as parser
+import BotConfig
 
 URL0 = 'https://rp5.ru'
 URL1 = '/Погода_в_мире'
 URL = URL0 + URL1
 
-countries_list, countries_links = prs.parse(URL) 
-print(countries_links)
+from aiogram import Bot, Dispatcher, executor, types
+bot = Bot(BotConfig.TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands="test1")
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
+
+
+executor.start_polling(dp, skip_updates=True)
+
+#countries_list, countries_links = parser.parse(URL) 
+#print(countries_links)
