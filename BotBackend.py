5e666a26dc7b56af696ef1cf7b049020a8e3f231
AntonioWanderer <areikow@mail.ru>
2022-06-30 00:52:59 +0700

    2-n parsing level, need to unify parsing alg

--- BotBackend.py
+++ BotBackend.py
@@ -7,7 +7,11 @@ from fuzzywuzzy import fuzz
 import BotConfig
 import GetWeather as Parser
 
-Last_msg = {}
+URL0 = 'https://rp5.ru'
+URL1 = '/Погода_в_мире'
+URL = URL0 + URL1
+
+Deep_id = {}
 
 bot = Bot(BotConfig.TOKEN)
 dp = Dispatcher(bot)
@@ -28,11 +32,35 @@ def BotMessage():
 	async def echo_message(msg: types.Message):
 		if msg.text == "Выбрать местоположение":
 			await bot.send_message(msg.from_user.id, "Введите страну")
-			Last_msg[UserId(msg)] = "Введите страну"
+			Deep_id[UserId(msg)] = "Страна"
 		else:
-			if Last_msg[UserId(msg)] == "Введите страну":
-				print("ok")
-				Last_msg[UserId(msg)] = " "
+			if Deep_id[UserId(msg)] == "Страна":
+				countries, countries_links = Parser.parse(1, URL)
+				high = []
+				mid = []
+				text = msg.text
+				for country in countries:
+					r = fuzz.ratio(country, text)
+					if r == 100:
+						high.append(country)
+					if r > 70:
+						mid.append(country)
+				if high == []:
+					await bot.send_message(msg.from_user.id, "Нет точных совпадений. Возможно, вы имели в виду что-то из:")
+					for c in mid:
+						await bot.send_message(msg.from_user.id, c)
+					if mid == []:
+						bot.send_message(msg.from_user.id, "Нет даже примерных совпадений, хм")
+				else:
+					
+					await bot.send_message(msg.from_user.id, "Страна найдена, введите регион")
+					Deep_id[UserId(msg)] = "Регион" + countries_links[countries.index(high[-1])]
+			
+			elif "Регион" in Deep_id[UserId(msg)]:
+				Url_reg = URL0 + Deep_id[UserId(msg)].replace("Регион","")
+				print(Url_reg)
+				
+				
 			else:
 				await bot.send_message(msg.from_user.id, "Excuse me, I don't understand")
   
@@ -41,5 +69,3 @@ def BotPolling():
 	
 def UserId(msg):
 	return(msg.chat.id)
-	
-	
